58ed461b7fed43aed4cf887db98d827dbd2d61ec
Brandon Stumbo <bstumbo1@gmail.com>
2020-02-20 20:37:11 -0600

    Setup url patterns for preceptors and students

--- practicum/urls.py
+++ practicum/urls.py
@@ -15,7 +15,27 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('/student', views.student, name='student'),
+    path('/student/practicum-plan', views.studentPlanForm, name='student-plan-form'),
+    path('/student/practicum-plan/<int:projectplan_id>', views.studentPlan, name='student-plan'),
+    path('/student/midpoint', views.studentMidpointForm, name='student-midpoint-form'),
+    path('/student/midpoint/<int:midpointevaluation_id', views.studentMidpoint, name='student-midpoint'),
+    path('/student/self-evaluation', views.studentSelfEvaluationForm, name='student-self-evaluation-form'),
+    path('/student/self-evaluation/<int:selfevaluation_id>', views.studentSelfEvaluation, name='student-self-evaluation'),
+    path('/student/preceptor-evaluation/<int:preceptor_evaluation_id>', views.preceptorSelfEvaluation,
+         name='preceptor-evaluation'),
+    path('/preceptor', views.preceptor, name='preceptor'),
+    path('/preceptor/practicum-plan/<int:practicum_plan_id>', views.preceptorPracticumPlanApproval,
+         name='preceptor-practicum-plan-approval'),
+    path('/preceptor/midpoint-evaluation/<int:midpointevaluation_id>', views.preceptorMidpointApproval,
+         name='preceptor-midpoint-approval'),
+    path('/preceptor/final-evaluation/<int:preceptor_final_evaluation_id>', views.preceptorFinalEvalution,
+         name='preceptor-final-evaluation'),
+    path('/practicum-director', view.practicum-director, name='practicum-director'),
+
 ]
