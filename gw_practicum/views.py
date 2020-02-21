0ff8e13a367b3ba1d1a64ec0629f73874b30b4d8
Brandon Stumbo <bstumbo1@gmail.com>
2020-02-21 13:12:43 -0600

    - Setup PD and preceptor views - Remove leading / from urls

--- gw_practicum/views.py
+++ gw_practicum/views.py
@@ -5,6 +5,9 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Homepage")
+"""
+    STUDENT VIEWS
+"""
 
 def student(request):
     response = "Student Homepage"
@@ -22,10 +25,58 @@ def studentSelfEvaluation(request, selfevaluation_id='None'):
     response = "Student Self Evaluation"
     return HttpResponse(response)
 
-def studentSelfEvaluation(request, selfevaluation_id='None'):
-    response = "Student Self Evaluation"
+def preceptorSelfEvaluation(request, preceptor_evaluation_id='None'):
+    response = "Student Preceptor Evaluation"
     return HttpResponse(response)
+
+"""
+    PRECEPTOR VIEWS
+"""
 
 def preceptorSelfEvaluation(request, preceptor_evaluation_id='None'):
     response = "Student Preceptor Evaluation"
+    return HttpResponse(response)
+
+def preceptor(request):
+    response = "Preceptor Homepage"
+    return HttpResponse(response)
+
+def preceptorPracticumPlanApproval(request, practicum_plan_id):
+    response = "Preceptor Practicum Plan Approval"
+    return HttpResponse(response)
+
+def preceptorMidpointApproval(request, midpointevaluation_i):
+    response = "Preceptor Midpoint Evaluation Approval"
+    return HttpResponse(response)
+
+def preceptorFinalEvalution(request, preceptor_final_evaluation_id):
+    response = "Preceptor Practicum Plan Approval"
+    return HttpResponse(response)
+
+"""
+    PRACTICUM DIRECTOR VIEWS
+"""
+
+def practicumDirector(request):
+    response = "Practicum Director Homepage"
+    return HttpResponse(response)
+
+def practicumDirectorPlanApproval(request, projectplan_id):
+    response = "Practicum Director Plan Approval"
+    return HttpResponse(response)
+
+def practicumMidpointEvaluationApproval(request, midpointevaluation_id):
+    response = "Practicum Director Midpoint Evaluation Approval"
+    return HttpResponse(response)
+
+def practicumFinalEvaluationApproval(request, finalevaluation_id):
+    response = "Practicum Director Final Evaluation Approval"
+    return HttpResponse(response)
+
+def siteRegistrationApproval(request, reg_id):
+    response = "PD Site Approval"
+    return HttpResponse(response)
+
+def preceptorRegistrationApproval(request, reg_id):
+    response = "PD Preceptor Approval"
     return HttpResponse(response)
