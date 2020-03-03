e78adab7b31b242a1ea3de0ee08a2e604c99b291
Brandon Stumbo <bstumbo1@gmail.com>
2020-03-02 21:02:10 -0600

    - Add ability to edit hours

--- gw_practicum/static/gw_practicum/custom/js/custom.js
+++ gw_practicum/static/gw_practicum/custom/js/custom.js
@@ -3,12 +3,18 @@
  */
 $('.close').on('click', function() {
   $('.modal').hide();
+  $('form#addHours').trigger('reset');
+    if ($('input#hours_id').length) {
+        $('input#hours_id').remove()
+    }
 });
 
+// Add Hours
 $('#addHoursButton').on('click', function () {
     $('#addHoursModal').toggle();
 });
 
+//Delete Hours
 $('.deleteHours').on('click', function() {
     hoursId = $(this).data('id');
     practicum = $(this).data('practicum');
@@ -21,6 +27,29 @@ $('.deleteHours').on('click', function() {
                 window.location.reload();
         }});
     });
+});
+
+//Edit Hours
+$('.editHours').on('click', function() {
+    hoursId = $(this).data('id');
+    $.ajax({
+        url: "/edit-hours/" + hoursId,
+        success: function(result){
+            console.log(result)
+            $('<input>', {
+                type: 'hidden',
+                id: 'hours_id',
+                name: 'hours_id',
+                value: hoursId
+            }).appendTo('form#addHours');
+
+            $('#id_start_date').val(result.hours.start_date);
+            $('#id_end_date').val(result.hours.end_date);
+            $('#id_hours').val(result.hours.hours);
+            $('#id_tasks').val(result.hours.tasks);
+            $('#hours_id').val(hoursId);
+            $('#addHoursModal').toggle();
+    }});
 });
 
 /*
